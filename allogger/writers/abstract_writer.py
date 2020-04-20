6e088463a40e39d30808fce41c5046cf7c8e6e56
Sebastian Blaes <sebastianblaes@gmail.com>
2020-04-20 12:30:09 +0200

    fixed bug in hdf_writers write_to_disc_function

--- allogger/writers/abstract_writer.py
+++ allogger/writers/abstract_writer.py
@@ -1,22 +1,11 @@
 from abc import ABC, abstractmethod
-from time import time as timestamp
-
-from .helpers import time
 
 class AbstractWriter(ABC):
 
     def __init__(self, filter='.*'):
-        self.init_time = timestamp()
-        self.last_write_time = self.init_time
         self.filter = filter
 
-    @property
-    def wall_time(self):
-        return timestamp() - self.init_time
 
-    @property
-    def time_diff_from_last_write(self):
-        return timestamp() - self.last_write_time
 
     @abstractmethod
     def add_scalar(self, key, value, step):
--- allogger/writers/hdfwriter.py
+++ allogger/writers/hdfwriter.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from time import time as timestamp
 
 from .abstract_writer import AbstractWriter
 from .helpers import gen_filename, time
@@ -7,7 +8,7 @@ from ..helpers import _acquire_lock, _release_lock
 
 def add_value_wrapper(f):
     def wrapper(self, key, *args, **kwargs):
-        if key not in self.data:
+        if (f.__name__.split('_')[-1], key) not in self.data:
             self.data[(f.__name__.split('_')[-1], key)] = []
 
         out = f(self, key, *args, **kwargs)
@@ -19,17 +20,30 @@ def add_value_wrapper(f):
     return wrapper
 
 class HDFWriter(AbstractWriter):
-    def __init__(self, output_dir, min_time_diff_btw_disc_writes=10, filter='.*'):
+    def __init__(self, output_dir, min_time_diff_btw_disc_writes=180, filter='.*'):
         super().__init__(filter=filter)
 
         self.output_dir = output_dir
         self.data = dict()
         self.min_time_diff_btw_disc_writes = min_time_diff_btw_disc_writes
         self.filename = gen_filename()
+        self.init_time = timestamp()
+        self.last_write_time = self.init_time
+
+    @property
+    def wall_time(self):
+        return timestamp() - self.init_time
+
+    @property
+    def time_diff_from_last_write(self):
+        return timestamp() - self.last_write_time
 
     def write_to_disc(self, force=False):
-        if self.min_time_diff_btw_disc_writes < self.min_time_diff_btw_disc_writes and not force:
+        if (timestamp() - self.last_write_time) < self.min_time_diff_btw_disc_writes and not force:
+            print(timestamp() - self.last_write_time, self.min_time_diff_btw_disc_writes, 'skip')
             return
+
+        print(timestamp() - self.last_write_time, self.min_time_diff_btw_disc_writes, 'do it')
 
         for (type, k), v in self.data.items():
             data = pd.DataFrame(v, columns=['step', 'time', 'wall_time', 'value']).set_index('step')
@@ -42,7 +56,7 @@ class HDFWriter(AbstractWriter):
                 _release_lock()
         self.data = dict()
 
-        self.last_write_time = time()
+        self.last_write_time = timestamp()
 
     def fixed_data_prefix(self, step):
         return [step, time(), self.wall_time]
